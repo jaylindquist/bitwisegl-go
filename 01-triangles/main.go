3aa1b403e512194a120d2627dc5aa1ae6c69dd5d
Jay Lindquist <jay.lindquist@gmail.com>
2021-07-08 21:02:02 -0500

    wrap app

--- 01-triangles/main.go
+++ 01-triangles/main.go
@@ -17,6 +17,11 @@ func init() {
 	runtime.LockOSThread()
 }
 
+type App struct {
+	numVertices int32
+	vbo uint32
+}
+
 func main() {
 	if err := glfw.Init(); err != nil {
 		log.Fatalln("failed to initialize glfw:", err)
@@ -38,31 +43,30 @@ func main() {
 	version := gl.GoStr(gl.GetString(gl.VERSION))
 	fmt.Println("OpenGL version", version)
 
-	initialize()
+	app := initialize()
 
 	for !window.ShouldClose() {
-		display()
+		app.Display()
 		window.SwapBuffers()
 		glfw.PollEvents()
 	}
 }
 
-const numVertices = 6
-var vbo uint32
+func initialize() App {
+	app := App{numVertices: 6}
 
-func initialize() {
 	var vao uint32
 	gl.GenVertexArrays(1, &vao)
 	gl.BindVertexArray(vao)
 
-	vertices := [6*2]float32{
-         -0.90, -0.90,   0.85, -0.90,  -0.90,  0.85,
-          0.90, -0.85,   0.90,  0.90,  -0.85,  0.90, 
+	vertices := [6][2]float32{
+         {-0.90, -0.90},  {0.85, -0.90},  {-0.90,  0.85},
+         { 0.90, -0.85},  {0.90,  0.90},  {-0.85,  0.90}, 
     }
 	
-    gl.CreateBuffers(1, &vbo)
-    gl.BindBuffer(gl.ARRAY_BUFFER, vbo)
-    gl.BufferStorage(gl.ARRAY_BUFFER, len(vertices) * 4, gl.Ptr(&vertices[0]), 0)
+    gl.CreateBuffers(1, &app.vbo)
+    gl.BindBuffer(gl.ARRAY_BUFFER, app.vbo)
+    gl.BufferStorage(gl.ARRAY_BUFFER, len(vertices) * 4 * 2, gl.Ptr(&vertices[0][0]), 0)
 
 	shaders := []bgl.ShaderInfo{
 		{ShaderType: gl.VERTEX_SHADER, Filename: "shaders/triangle.vert"},
@@ -79,12 +83,14 @@ func initialize() {
 	vPosition := uint32(gl.GetAttribLocation(program, gl.Str("vPosition\x00")))
     gl.VertexAttribPointerWithOffset(vPosition, 2, gl.FLOAT, false, 2*4, 0)
     gl.EnableVertexAttribArray(vPosition)
+
+	return app
 }
 
-func display() {
+func (app *App) Display() {
 	black := [4]float32{0.0, 0.0, 0.0, 0.0}
 
 	gl.ClearBufferfv(gl.COLOR, 0, &black[0])
-	gl.BindVertexArray(vbo)
-	gl.DrawArrays(gl.TRIANGLES, 0,numVertices)
+	gl.BindVertexArray(app.vbo)
+	gl.DrawArrays(gl.TRIANGLES, 0, app.numVertices)
 }
